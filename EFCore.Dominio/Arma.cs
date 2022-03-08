43adeb2163cd20fcc7f87432fa294574df6bb0d1
liimarb1 <98990063+liimarb1@users.noreply.github.com>
2022-03-08 17:44:46 -0300

    Inserindo Camadas e mostrando o CRUD básico!

--- EFCore.Dominio/Arma.cs
+++ EFCore.Dominio/Arma.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebAPI.Models
+namespace EFCore.Dominio
 {
-    public class IdentidadeSecreta
+    public class Arma
     {
         public int Id { get; set; }
-        public int NomeReal { get; set; }
-        public int HeroiId { get; set; }
+        public string Nome { get; set; }
         public Heroi Heroi { get; set; }
-        
+        public int HeroiId { get; set; }
 
     }
 }
--- EFCore.Repo/HeroiContext.cs
+++ EFCore.Repo/HeroiContext.cs
@@ -1,23 +1,21 @@
-﻿using EFCore.WebAPI.Models;
+﻿using EFCore.Dominio;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EFCore.WebAPI.Data
+namespace EFCore.Repo
 {
     public class HeroiContext : DbContext
     {
+        public HeroiContext(DbContextOptions<HeroiContext> options) : base(options) {}
         public DbSet<Arma> Armas { get; set; }
         public DbSet<Batalha> Batalhas { get; set; }
         public DbSet<Heroi> Herois { get; set; }
         public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
         public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Password=sa123456;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=DESKTOP-9M0RIHB");
-        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<HeroiBatalha>(entity =>
