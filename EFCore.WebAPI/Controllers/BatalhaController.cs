0f7fd82ced6e6893a9f4b5c471cbb0b2a899e430
liimarb1 <98990063+liimarb1@users.noreply.github.com>
2022-03-15 11:49:41 -0300

    Controllers Heroi e Batalha e novo migration

---
+++ EFCore.WebAPI/Controllers/BatalhaController.cs
@@ -0,0 +1,96 @@
+﻿using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCore.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatalhaController : ControllerBase
+    {
+
+        private readonly HeroiContext _context;
+
+        public BatalhaController(HeroiContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<Batalha>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+
+            }
+        }
+
+        // GET api/<Batalha>/5
+        [HttpGet("{id}", Name = "GetBatalha")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Batalha
+        [HttpPost]
+        public ActionResult Post(Batalha model)
+        {
+            try
+            {
+                _context.Batalhas.Add(model);
+                _context.SaveChanges();
+
+                return Ok("BAZINGA");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // PUT api/Batalha/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Batalha model)
+        {
+            try
+            {
+                if (_context.Batalhas
+                    .AsNoTracking()
+                    .FirstOrDefault(h => h.Id == id) != null)
+                {
+                    _context.Update(model);
+                    _context.SaveChanges();
+
+                    return Ok("BAZINGA");
+                }
+                return Ok("Não Encontrado!");
+            
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+    
+
+        // DELETE api/<Batalha/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
